53f1f69509de9ec81c7096a836e8b8f7dff95f49
Mohid Khan <mohidkhan169@gmail.com>
2025-11-08 14:44:15 -0800

    adding animation

--- experiments/test_rollout.py
+++ experiments/test_rollout.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from envs.cartpole import CartPole
+import matplotlib.animation as animation
+
 
 # --- setup ---
 env = CartPole()  # use default parameters
@@ -41,3 +43,51 @@ ax[1].set_ylabel("Action")
 ax[1].set_xlabel("Time (s)")
 plt.tight_layout()
 plt.show()
+
+def animate_cartpole(X, dt):
+    # unpack
+    x = X[:, 0]    # cart position
+    th = X[:, 2]   # pole angle
+
+    # geometry constants
+    l = env.l        # pole length
+    cart_w = 0.2
+    cart_h = 0.1
+
+    fig, ax = plt.subplots(figsize=(6, 3))
+    ax.set_xlim(-env.L/2 - 0.5, env.L/2 + 0.5)
+    ax.set_ylim(-l - 0.1, l + 0.1)
+    ax.set_xlabel("Track position (m)")
+    ax.set_ylabel("Height (m)")
+    ax.set_aspect('equal', 'box')
+
+    # static base line
+    ax.plot([-env.L/2, env.L/2], [0, 0], "k-", lw=2)
+
+    # elements to animate
+    cart_patch = plt.Rectangle((0, 0), cart_w, cart_h, color="tab:blue", ec="k")
+    pole_line, = ax.plot([], [], "o-", color="tab:orange", lw=2, markersize=6)
+    ax.add_patch(cart_patch)
+
+    def init():
+        cart_patch.set_xy((-cart_w/2, -cart_h/2))
+        pole_line.set_data([], [])
+        return cart_patch, pole_line
+
+    def update(frame):
+        # cart coordinates
+        xc = x[frame]
+        yc = 0
+        cart_patch.set_xy((xc - cart_w/2, yc - cart_h/2))
+
+        # pole end
+        xp = xc + l * np.sin(th[frame])
+        yp = yc + l * np.cos(th[frame])
+        pole_line.set_data([xc, xp], [yc, yp])
+        return cart_patch, pole_line
+
+    ani = animation.FuncAnimation(fig, update, frames=len(x), init_func=init,
+                                  blit=True, interval=dt*1000)
+    plt.show()
+
+animate_cartpole(X, dt)
